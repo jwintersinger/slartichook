5c48ee8dc02604336d47f66fe0bf060e59a0e767
Jeff Wintersinger <jeff+git@micand.com>
2010-06-25 19:02:32 -0600

    Paginate blog posts.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
-from django.http import HttpResponse
+from django.core import paginator, urlresolvers
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from blog.models import Post
-from google.appengine.ext import db
 
-def post_list(request):
+def post_list(request, page=None):
+  #_create_test_post()
+  posts = Post.objects.all()
+  posts.order('-created_at')
+
+  if not page:
+    page = 1
+  pgnr = paginator.Paginator(posts, 3)
+  try:
+    paginated_posts = pgnr.page(page)
+  except (paginator.EmptyPage, paginator.InvalidPage):
+    return HttpResponseRedirect(urlresolvers.reverse('blog.views.post_list'))
+
+  return render_to_response('blog/post_list.html', {'posts': paginated_posts})
+
+def _create_test_post():
+  from google.appengine.ext import db
   p = Post(
     title = 'Happy happy',
     body = 'Socks.\n\n* Whoa\n* Yeah',
@@ -12,10 +28,6 @@ def post_list(request):
     slug = 'slug',
   )
   p.put()
-
-  posts = Post.objects.all()
-  posts.order('-created_at')
-  return render_to_response('blog/post_list.html', {'posts': posts})
 
 def post_detail(request, slug):
   post = Post.objects.all()
