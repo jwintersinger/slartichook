99b176504c5a6072bb06d0975e3298901070d537
Jeff Wintersinger <jeff+git@micand.com>
2010-06-25 18:18:02 -0600

    Add ability to filter archive by tag.

--- blog/views.py
+++ blog/views.py
@@ -8,17 +8,27 @@ def post_list(request):
     title = 'Happy happy',
     body = 'Socks.',
     #user = users.get_current_user(),
-    tags = [db.Category('happiness')],
+    tags = [db.Category('bonners')],
     slug = 'slug',
   )
   p.put()
 
-  return render_to_response('blog/post_list.html', {'posts': Post.objects.all()})
+  posts = Post.objects.all()
+  posts.order('-created_at')
+  return render_to_response('blog/post_list.html', {'posts': posts})
 
 def post_detail(request, slug):
   post = Post.objects.all()
   post.filter('slug =', slug)
   return render_to_response('blog/post_detail.html', {'post': post.fetch(1)[0]})
 
-def post_archive(request):
-  return render_to_response('blog/post_archive.html', {'posts': Post.objects.all()})
+def post_archive(request, tag=None):
+  posts = Post.objects.all()
+  posts.order('-created_at')
+  if tag:
+    posts.filter('tags =', tag)
+
+  return render_to_response('blog/post_archive.html', {
+    'posts': posts,
+    'tag':   tag
+  })
