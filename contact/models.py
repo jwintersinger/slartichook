934eb9900df582b69e65f3df8e0636dac87b5bf4
Jeff Wintersinger <jeff+git@micand.com>
2010-08-04 15:27:58 -0600

    Store contact attempts in datastore as backup.

--- contact/models.py
+++ contact/models.py
@@ -1,4 +1,8 @@
 from appengine_django.models import BaseModel
 from google.appengine.ext import db
 
-# Create your models here.
+class Missive(BaseModel):
+  name       = db.StringProperty(required=True)
+  email      = db.EmailProperty(required=True)
+  message    = db.TextProperty(required=True)
+  created_at = db.DateTimeProperty(required=True, auto_now_add=True)
--- contact/views.py
+++ contact/views.py
@@ -2,34 +2,47 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from google.appengine.api.mail import send_mail
-from contact.forms import ContactForm
 from miscellany.utils import render_email
 from settings import CONTACT_TO
+from contact.forms import ContactForm
+from contact.models import Missive
 
 def contact(request):
   if request.method == 'POST':
     form = ContactForm(request.POST)
     if form.is_valid():
-      data = form.cleaned_data
-      subject, message = render_email(
-        'contact/email_subject.html',
-        'contact/email_message.html',
-        message      = data['message'],
-        from_name    = data['name'],
-        from_address = data['email']
-      )
-      # BUG: with appengine_django, one should be able to use
-      # django.core.mail.send_mail. This only works for Django 1.1, however --
-      # Django 1.2 throws a variety of exceptions, as appengine_django hasn't
-      # been properly updated for Django 1.2's mail code. Thus, I must use the
-      # native mail API built into App Engine.
-      send_mail(data['email'], CONTACT_TO, subject, message)
+      form_data = form.cleaned_data
+      _send_contact_email(form_data)
+      _store_contact(form_data)
       return HttpResponseRedirect(reverse('contact.views.contact_confirmation'))
-
   else:
     form = ContactForm()
-
   return render_to_response('contact/form.html', {'form': form})
 
 def contact_confirmation(request):
   return render_to_response('contact/confirmation.html')
+
+def _send_contact_email(form_data):
+  subject, message = render_email(
+    'contact/email_subject.html',
+    'contact/email_message.html',
+    message      = form_data['message'],
+    from_name    = form_data['name'],
+    from_address = form_data['email']
+  )
+  # BUG: with appengine_django, one should be able to use
+  # django.core.mail.send_mail. This only works for Django 1.1, however --
+  # Django 1.2 throws a variety of exceptions, as appengine_django hasn't
+  # been properly updated for Django 1.2's mail code. Thus, I must use the
+  # native mail API built into App Engine.
+  return send_mail(form_data['email'], CONTACT_TO, subject, message)
+
+# Store contact on the off-chance that something goes awry with the e-mail
+# sending process.
+def _store_contact(form_data):
+  missive = Missive(
+    name    = form_data['name'],
+    email   = form_data['email'],
+    message = form_data['message']
+  )
+  return missive.put()
