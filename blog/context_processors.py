6453f1431e7da27ba57eb5178852a5ae23d4fbee
Jeff Wintersinger <jeff.git@wintersinger.com>
2010-08-21 02:12:46 -0600

    Make recent posts sort inverse chronologically.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,6 +1,8 @@
 from blog.models import Post
 
 def recent_posts(request):
+  recent_posts = Post.all()
+  recent_posts.order('-created_at')
   return {
-    'recent_posts': Post.all().fetch(5),
+    'recent_posts': recent_posts.fetch(5),
   }
