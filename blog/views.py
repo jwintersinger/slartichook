c0828994f7b653deb0ccf63e4742b61ca55146aa
Jeff Wintersinger <jeff.git@wintersinger.com>
2010-08-21 02:53:30 -0600

    Update test blog post creation.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.views.generic.simple import direct_to_template
 from blog.models import Post
 
 def post_list(request, page=None):
-  #_create_test_post()
+  #_create_test_posts()
   posts = Post.visible()
 
   if not page:
@@ -18,20 +18,6 @@ def post_list(request, page=None):
   return direct_to_template(request, 'blog/post_list.html', {
     'paginated_posts': paginated_posts,
   })
-
-def _create_test_post():
-  from google.appengine.ext import db
-  from django.contrib.webdesign import lorem_ipsum
-  from datetime import datetime
-  p = Post(
-    title = 'Happy happy',
-    body = (2*'\n').join(lorem_ipsum.paragraphs(4)),
-    #user = users.get_current_user(),
-    tags = [db.Category('bonners'), db.Category('cheese'), db.Category('weiners')],
-    slug = 'phallus',
-    published_at = datetime.fromtimestamp(1282380470 - 365*24*60*60),
-  )
-  p.put()
 
 def post_detail(request, slug):
   post = Post.visible()
@@ -48,3 +34,26 @@ def post_archive(request, tag=None):
     'posts': posts,
     'tag':   tag
   })
+
+def _create_test_post(slug, time_delta):
+  from google.appengine.ext import db
+  from django.contrib.webdesign import lorem_ipsum
+  from datetime import datetime
+  p = Post(
+    title         = 'Happy happy',
+    body          = (2*'\n').join(lorem_ipsum.paragraphs(4)),
+    #user          = users.get_current_user(),
+    tags          = [
+      db.Category('bonners'),
+      db.Category('cheese'),
+      db.Category('weiners')
+    ],
+    slug          = slug,
+    published_at = datetime.fromtimestamp(1282380470 - time_delta),
+  )
+  p.put()
+
+def _create_test_posts():
+  _create_test_post('phallus', 365*24*60*60)
+  _create_test_post('weiner', 180*24*60*60)
+  _create_test_post('peanut_butter', 5*60)
